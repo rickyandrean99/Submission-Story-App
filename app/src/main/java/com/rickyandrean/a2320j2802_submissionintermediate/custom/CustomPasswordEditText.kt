ff7bc19882bf6a1561caf011cbf132ef0e9e6274
rickyandrean99 <ricky.ralph09@gmail.com>
2022-04-15 22:38:20 +0700

    Progress custom password

--- app/src/main/java/com/rickyandrean/a2320j2802_submissionintermediate/custom/CustomPasswordEditText.kt
+++ app/src/main/java/com/rickyandrean/a2320j2802_submissionintermediate/custom/CustomPasswordEditText.kt
@@ -5,19 +5,27 @@ import android.graphics.Canvas
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.util.Log
 import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.rickyandrean.a2320j2802_submissionintermediate.R
 
-class CustomPasswordEditText : AppCompatEditText {
-    private lateinit var activeBackground: Drawable
+
+class CustomPasswordEditText : AppCompatEditText, View.OnTouchListener {
+    // private lateinit var activeBackground: Drawable
     private lateinit var inactiveBackground: Drawable
+    private lateinit var validBackground: Drawable
+    private lateinit var invalidBackground: Drawable
     private lateinit var visibilityOn: Drawable
     private lateinit var visibilityOff: Drawable
+    private lateinit var visibilityImage: Drawable
+    private lateinit var passwordImage: Drawable
     private var valid: Boolean = false
     private var show: Boolean = false
 
@@ -38,14 +46,24 @@ class CustomPasswordEditText : AppCompatEditText {
     }
 
     private fun init() {
+        // Image drawable initialization
         activeBackground =
             ContextCompat.getDrawable(context, R.drawable.bg_password_active) as Drawable
         inactiveBackground =
             ContextCompat.getDrawable(context, R.drawable.bg_password_inactive) as Drawable
         visibilityOn = ContextCompat.getDrawable(context, R.drawable.ic_visibility_on) as Drawable
         visibilityOff = ContextCompat.getDrawable(context, R.drawable.ic_visibility_off) as Drawable
+        passwordImage = ContextCompat.getDrawable(context, R.drawable.ic_key) as Drawable
 
+        // Determine background and icon for the first time
+        visibilityImage = visibilityOff
         background = inactiveBackground
+
+        // Hide password text for the first time
+        inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        // To show the key icon for the first time
+        setButtonDrawables()
 
         addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -60,20 +78,17 @@ class CustomPasswordEditText : AppCompatEditText {
                 // Do nothing
             }
         })
+
+        setOnTouchListener(this)
     }
 
     private fun setButtonDrawables(
-        startOfTheText: Drawable? = null,
-        topOfTheText: Drawable? = null,
-        endOfTheText: Drawable? = null,
-        bottomOfTheText: Drawable? = null
+        start: Drawable? = passwordImage,
+        top: Drawable? = null,
+        end: Drawable? = null,
+        bottom: Drawable? = null
     ) {
-        setCompoundDrawablesWithIntrinsicBounds(
-            startOfTheText,
-            topOfTheText,
-            endOfTheText,
-            bottomOfTheText
-        )
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, bottom)
     }
 
     private fun validatePassword(text: String?) {
@@ -82,9 +97,26 @@ class CustomPasswordEditText : AppCompatEditText {
         }
     }
 
+    private fun changeVisibility() {
+        visibilityImage = if (show) visibilityOff else visibilityOn
+        show = !show
+
+        // Change visibility type of edit text
+        inputType = if (show) {
+            InputType.TYPE_CLASS_TEXT
+        } else {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        }
+
+        // When visibility changed, the cursor is at the very end of the sentence
+        setSelection(this.length())
+    }
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         hint = "Password"
+        typeface = ResourcesCompat.getFont(context, R.font.roboto_medium)
+        setTextColor(resources.getColor(R.color.black))
         setTextSize(TypedValue.COMPLEX_UNIT_SP, 16F)
     }
 
@@ -93,10 +125,48 @@ class CustomPasswordEditText : AppCompatEditText {
 
         if (focused) {
             background = activeBackground
-            setButtonDrawables(endOfTheText = if (show) visibilityOn else visibilityOff )
+            setButtonDrawables(end = visibilityImage)
         } else {
             background = inactiveBackground
             setButtonDrawables()
         }
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        if (compoundDrawables[2] != null) {
+            val visibilityButtonStart: Float
+            val visibilityButtonEnd: Float
+            var isVisibilityButtonClicked = false
+
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                visibilityButtonEnd = (visibilityImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event.x < visibilityButtonEnd -> isVisibilityButtonClicked = true
+                }
+            } else {
+                visibilityButtonStart =
+                    (width - paddingEnd - visibilityImage.intrinsicWidth).toFloat()
+                when {
+                    event.x > visibilityButtonStart -> isVisibilityButtonClicked = true
+                }
+            }
+
+            if (isVisibilityButtonClicked) {
+                return when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        setButtonDrawables(end = visibilityImage)
+                        true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        changeVisibility()
+                        setButtonDrawables(end = visibilityImage)
+                        true
+                    }
+                    else -> false
+                }
+            }
+            return false
+        }
+        return false
     }
 }
